a1e9fd96245fcc9d0654b04cb183a3808f6ff914
lk <919745602@qq.com>
2016-05-07 16:28:14 +0800

    加油

---
+++ Douban_8/Douban_8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Douban_8
+//
+//  Created by lk on 16/5/7.
+//  Copyright © 2016年 唐家文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Douban_8/Douban_8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Douban_8
+//
+//  Created by lk on 16/5/7.
+//  Copyright © 2016年 唐家文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
